7f816ba64c26618d2af51d5fffecbc874ede9dff
alice137037 <dededuud@163.com>
2024-07-31 15:13:22 +0800

    deploy

--- obs_sctipts/NewBlog.js
+++ obs_sctipts/NewBlog.js
@@ -2,7 +2,7 @@ const util = require("util");
 const child_process = require("child_process");
 const exec = util.promisify(child_process.exec);
 
-//新建博客命名，这里以年月日时分秒命名，可自定义修改
+// 新建博客命名，这里以年月日时分秒命名，可自定义修改
 function getCreateTimeAsFileName() {
   var d = new Date();
   var year = d.getFullYear();
@@ -11,17 +11,16 @@ function getCreateTimeAsFileName() {
   var hour = d.getHours();
   var minute = d.getMinutes();
   var second = d.getSeconds();
-  var time =  year + "-" + month +"-" + day +  "day" + hour +  "h" + minute +  "m" + second +  "s";
+  var time = year + "-" + month + "-" + day + "day" + hour + "h" + minute + "m" + second + "s";
 
   return time;
 }
 
-
-// execute command function
-
+// 执行命令函数
 async function executeCommand() {
   const fileName = getCreateTimeAsFileName() + ".md";
-  const { stdout, stderr } = await exec("hugo new posts/" + fileName, {
+  const hugoPath = "D:\\hugo\\bin\\hugo.exe"; // 替换为hugo的实际路径
+  const { stdout, stderr } = await exec(`${hugoPath} new posts/${fileName}`, {
     cwd: app.fileManager.vault.adapter.basePath,
   });
   console.log("stdout:", stdout);
@@ -29,7 +28,7 @@ async function executeCommand() {
   if (stdout) {
     new Notice("New Blog Created[" + fileName + "]");
   } else {
-    new Notice("New Blog Create Faild. " + stderr);
+    new Notice("New Blog Create Failed. " + stderr);
   }
 }
 
